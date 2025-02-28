6acdf14325575bc0c804e54376473aefc90ee9e9
Robertas Zamblauskas <robertas.zamblauskas@gmail.com>
2025-02-28 14:03:21 +0200

    Added zio-3-test

---
+++ zio-3-test/src/test/scala/org/example/Spec.scala
@@ -0,0 +1,15 @@
+package org.example
+
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class Spec extends AnyFunSpec with Matchers with TypeCheckedTripleEquals {
+
+  describe("Spec") {
+    it("should do something") {
+      1 should === (1)
+    }
+  }
+
+}
